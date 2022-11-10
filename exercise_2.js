3f611616da7c6efadfb07162d6758928e6582239
holosexecartisano <ecartisano@holosglobal.com>
2022-11-10 18:42:59 -0300

    exercise_2

---
+++ exercise_2.js
@@ -0,0 +1,13 @@
+let fullName = "Matías Exequiel Cartisano";
+
+var age = 33;
+
+let Isdeveloper = true;
+
+let birthDate = new Date(1989,06,21);
+
+var favoriteBook = "Ami El Niño De Las Estrellas";
+
+personalInformation = [fullName, age, Isdeveloper, birthDate, favoriteBook];
+
+console.log(personalInformation);
